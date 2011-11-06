3afb54a0f26e8bd6f69ab601245622b9c1188d29
Brett Davis <hctv19@gmail.com>
2011-11-06 01:25:36 +0000

    Fixing new message count

--- src/controller/MessageController.py
+++ src/controller/MessageController.py
@@ -69,7 +69,7 @@ class MessageController:
     def get_new_message_count(self, format="json", **kwargs):
         user, sMessages, fMessages, newMessageCount = cherrypy.session.get("user"), [], [], []
         try:
-            newMessageCount = session.query(func.count(MessageShare.message_id), MessageShare.recipient_id,).filter(and_(MessageShare.recipient_id==user.id, MessageShare.date_viewed is not None)).scalar()
+            newMessageCount = session.query(func.count('*')).select_from(MessageShare).filter(and_(MessageShare.recipient_id==user.id, MessageShare.date_viewed==None)).scalar()
         except Exception,e :
             fMessages.append(str(e))
         return fl_response(sMessages, fMessages, format, data=newMessageCount)
