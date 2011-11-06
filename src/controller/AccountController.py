9816d9ab30fb3918179c25c251e03ef863fcef63
Brett Davis <hctv19@gmail.com>
2011-11-06 05:51:31 +0000

    debug

--- src/controller/AccountController.py
+++ src/controller/AccountController.py
@@ -187,6 +187,19 @@ class AccountController:
             fMessages.append("Could not get groups: %s" % str(e))
         return fl_response(sMessages, fMessages, format, data=groups)
 
+    def get_group_members(self, groupId, **kwargs):
+        user, sMessages, fMessages = (cherrypy.session.get("user"),  [], [])
+        try:
+            searchWidget = get_search_widget("manage_groups")
+            templateFile = fl.get_template_file('view_group.tmpl')
+                groupId = strip_tags(groupId)
+            group = session.query(Group).filter(Group.id == groupId).one()
+            if group.owner_id == user.id or user_has_permission(user, "admin"):
+                pass
+        except Exception, e:
+            pass
+        tpl = Template(file=get_template_file('files.tmpl'), searchList=[locals(),globals()])
+        return str(tpl)
     @cherrypy.expose
     @cherrypy.tools.requires_login(permission="admin")
     def create_role(self, roleId, roleName, email, quota, format="json", **kwargs):
