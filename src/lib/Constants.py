91c1951bea1619ea314b381cb225829b8b90f585
Brett Davis <hctv19@gmail.com>
2012-01-25 04:11:44 +0000

    Constants

---
+++ src/lib/Constants.py
@@ -0,0 +1,11 @@
+
+__author__="wbdavis"
+__date__ ="$Jan 23, 2012 11:49:31 PM$"
+
+if __name__ == "__main__":
+    print "Hello";
+
+class Actions:
+    DOWNLOAD = "Download File"
+    UPLOAD = "Check In File"
+    UPLOAD_REQUEST_FULFILLED = "Upload Requested File"
