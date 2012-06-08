5103e5be2ea41331a2b110533f6275be43b9a0e0
Brett Davis <hctv19@gmail.com>
2012-06-08 19:43:39 +0000

    Removed foreign key constraints on audit log initiator and affected user ids. This would cause problems in certain cases and would also cause problems if  user were to be deleted, resulting in all associated logs to be deleted.

--- src/lib/Models.py
+++ src/lib/Models.py
@@ -380,12 +380,12 @@ class ClusterNode(Base):
 class AuditLog(Base):
     __tablename__ = "audit_logs"
     id = Column(Integer, primary_key=True)
-    initiator_user_id = Column(String(30), ForeignKey("users.id"), nullable=False)
+    initiator_user_id = Column(String(30),  nullable=False)
     action = Column(String(255), nullable=False)
     affected_user_id = Column(String(30), ForeignKey("users.id"))
     message = Column(Text, nullable=False)
     date = Column(DateTime, nullable=False)
-    affected_role_id = Column(String(30), ForeignKey("roles.id"), nullable=True)
+    affected_role_id = Column(String(30),  nullable=True)
     file_id = Column(Integer, nullable=True)
 
     display_class = None
